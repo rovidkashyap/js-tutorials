76ba64768f779947b8f740d49bf80bb406ed3433
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-01-19 15:35:54 +0530

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 154859
+let accountEmail = "rovid.kashyap@gmail.com"
+var accountPassword = "DevRovid123"
+accountCity = "Agra"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "123456"
+accountCity = "Delhi"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
