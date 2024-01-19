2dd4546fb62b921d039d98183901641815bcf875
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-01-19 15:48:34 +0530

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Rovid";
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Rovid"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Rovid" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
