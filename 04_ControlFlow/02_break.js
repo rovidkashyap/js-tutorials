8b504cbc61279a16eade27374adda0afa71b6687
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-01-24 13:17:36 +0530

    breakControlFlow

---
+++ 04_ControlFlow/02_break.js
@@ -0,0 +1,12 @@
+const fruit = "Apple";
+
+switch (fruit) {
+    case "Apple":
+        console.log("Apple is healthy for our body");
+        break;
+    case "Mango":
+        console.log("Mango is a National fruit of India");
+        break;
+    default:
+        console.log("I don't like fruits.");
+}
