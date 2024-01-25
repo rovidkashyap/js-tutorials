eb2f1dfe910444e1e51fe35cb2b35a4796c4be40
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-01-25 12:52:56 +0530

    Assignment Operators

---
+++ 06_Operators/01_assignmentOperators.js
@@ -0,0 +1,20 @@
+// Let take some variable
+
+// let a = 2;
+// const b = 3;
+
+// console.log(a); // 2
+
+// console.log(a = b + 1); // 4
+
+// Reminder Assignment
+let a = 10;
+let b = 5;
+
+// console.log(yoo %= 12);
+
+// Exponential Operator
+// console.log(a **= b);
+
+// Left Sift Assignment
+console.log(a<<=b);
